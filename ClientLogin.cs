a228d080ceb352de4385bee02e7ee01f653a3aef
emberfarkas <hubing91@163.com>
2016-01-18 12:06:16 +0800

    add plugins

--- ClientLogin.cs
+++ ClientLogin.cs
@@ -11,14 +11,16 @@ public class ClientLogin : MonoBehaviour
 
     private Socket Sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
     private Dictionary<string, string> Token = new Dictionary<string, string>();
-    private string last = "";
-    private Queue<string> Lines = new Queue<string>();
+    private byte[] Last = null;
+    private byte[] RecvBuffer = new byte[1024];
+    private int RecvBufferOffset = 0;
+    private int RecvBufferSz = 0;
     private byte[] challenge = null;
     private byte[] clientkey = null;
     private byte[] secret = null;
     private byte[] subid = null;
     private int step = 0;
-    private byte[] recvBuffer = new byte[1024];
+    private bool Connected = false;
 
     // Use this for initialization
     void Start()
@@ -35,102 +37,74 @@ public class ClientLogin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Sock.Poll(0, SelectMode.SelectWrite))
+        if (!Connected)
         {
-            
+            return;
         }
-        if (step == 1)
+        if (Sock.Poll(0, SelectMode.SelectRead))
         {
-            Sock.BeginReceive(recvBuffer, 0, 1024, SocketFlags.None, RecvChanlenge, this);
-            step = 0;
+            int ret = Sock.Receive(RecvBuffer, RecvBufferSz, 1024 - RecvBufferSz, SocketFlags.None);
+            RecvBufferSz += ret;
         }
-        if (step == 2)
+        if (RecvBufferOffset < RecvBufferSz && RecvBufferSz < 1024)
         {
-            Sock.BeginReceive(recvBuffer, 0, 1024, SocketFlags.None, RecvSecret, this);
-            step = 0;
+            if (step == 1)
+            {
+
+                byte[] buffer = ReadLine();
+                challenge = Crypt.base64decode(buffer);
+                clientkey = Crypt.randomkey();
+                WriteLine(Crypt.base64encode(Crypt.dhexchange(clientkey)));
+                step = 2;
+                return;
+            }
+            else if (step == 2)
+            {
+                byte[] buffer = ReadLine();
+
+                byte[] key = Crypt.base64decode(buffer);
+                secret = Crypt.dhsecret(key, clientkey);
+                Debug.Log("sceret is " + Encoding.ASCII.GetString(Crypt.hexencode(secret)));
+                byte[] hmac = Crypt.hmac64(challenge, secret);
+                WriteLine(Crypt.base64encode(hmac));
+                WriteToke();
+                step = 3;
+                return;
+            }
+            else if (step == 3)
+            {
+                byte[] buffer = ReadLine();
+                string str = Encoding.ASCII.GetString(buffer);
+                int code = Int32.Parse(str.Substring(0, 3));
+                Debug.Assert(code == 200);
+                Sock.Close();
+                string en = str.Substring(4, 4);
+                subid = Crypt.base64decode(Encoding.ASCII.GetBytes(en));
+                step = 4;
+            }
+            else if (step == 4)
+            {
+                var com = GameObject.Find("Agent").GetComponent<ClientAgent>();
+                com.StartConnServer(secret, subid);
+                step = 0;
+            }
         }
-        else if (step == 3)
+        else
         {
-            Sock.BeginReceive(recvBuffer, 0, 1024, SocketFlags.None, RecvResult, this);
-            step = 0;
-        }
-        else if (step == 4)
-        {
-            var ca = GameObject.Find("Agent").GetComponent<ClientAgent>();
-            ca.StartConnServer(secret, subid);
-            step = 0;
+            if (RecvBufferSz > 0)
+            {
+                Array.Copy(RecvBuffer, RecvBufferOffset, RecvBuffer, 0, RecvBufferSz);
+            }
         }
     }
 
-    private void ConnectAC(IAsyncResult ar)
+    void ConnectAC(IAsyncResult ar)
     {
         Sock.EndConnect(ar);
-        step = 1;
+        Connected = true;
     }
 
-    private void RecvChanlenge(IAsyncResult ar)
-    {
-        Sock.EndReceive(ar);
-        int i = 0;
-        for (; i < recvBuffer.Length; i++)
-        {
-            if (recvBuffer[i] == 10)
-            {
-                break;
-            }
-        }
-        byte[] buffer = new byte[i];
-        Array.Copy(recvBuffer, buffer, i);
-        recvBuffer = new byte[1024];
-        challenge = Crypt.base64decode(buffer);
-        clientkey = Crypt.randomkey();
-        string rt = Encoding.ASCII.GetString(Crypt.base64encode(Crypt.dhexchange(clientkey)));
-        WriteLine(rt);
-        step = 2;
-    }
-
-    private void RecvSecret(IAsyncResult ar)
-    {
-        Sock.EndReceive(ar);
-        int i = 0;
-        for (; i < recvBuffer.Length; i++)
-        {
-            if (recvBuffer[i] == 10)
-            {
-                break;
-            }
-        }
-        byte[] buffer = new byte[i];
-        Array.Copy(recvBuffer, buffer, i);
-        recvBuffer = new byte[1024];
-
-        byte[] key = Crypt.base64decode(buffer);
-        secret = Crypt.dhsecret(key, clientkey);
-        Debug.Log("sceret is " + Encoding.ASCII.GetString(Crypt.hexencode(secret)));
-        byte[] hmac = Crypt.hmac64(challenge, secret);
-        WriteLine(Encoding.ASCII.GetString(Crypt.base64encode(hmac)));
-        Dictionary<string, string> token = new Dictionary<string, string>();
-        token["server"] = "sample";
-        token["hello"] = "hello";
-        token["pass"] = "password";
-        WriteToke(token);
-        step = 3;
-    }
-
-    private void RecvResult(IAsyncResult ar)
-    {
-        Sock.EndReceive(ar);
-        string str = Encoding.ASCII.GetString(recvBuffer);
-        recvBuffer = new byte[1024];
-        int code = Int32.Parse(str.Substring(0, 3));
-        Debug.Assert(code == 200);
-        Sock.Close();
-        string en = str.Substring(4, 4);
-        subid = Crypt.base64decode(Encoding.ASCII.GetBytes(en));
-        step = 4;
-    }
-
-    public void WriteToke(Dictionary<string, string> token)
+    public void WriteToke()
     {
         string str = String.Format("{0}@{1}:{2}", Encoding.ASCII.GetString(Crypt.base64encode(Encoding.ASCII.GetBytes("hello"))),
             Encoding.ASCII.GetString(Crypt.base64encode(Encoding.ASCII.GetBytes("sample"))),
@@ -138,26 +112,30 @@ public class ClientLogin : MonoBehaviour
         Debug.Log(str);
         byte[] etoken = Crypt.desencode(secret, Encoding.ASCII.GetBytes(str));
         byte[] b = Crypt.base64encode(etoken);
-        WriteLine(Encoding.ASCII.GetString(b));
+        WriteLine(b);
     }
 
-    private void WriteLine(string text)
+    private byte[] ReadLine()
     {
-        text += "\n";
-        byte[] buffer = Encoding.ASCII.GetBytes(text);
-        Send(buffer);
+        int i = 0;
+        for (i = RecvBufferOffset; i < RecvBufferSz; i++)
+        {
+            if (RecvBuffer[i] == 10)
+            {
+                break;
+            }
+        }
+        byte[] buffer = new byte[i - RecvBufferOffset];
+        Array.Copy(RecvBuffer, RecvBufferOffset, buffer, 0, i - RecvBufferOffset);
+        RecvBufferOffset += (i - RecvBufferOffset);
+        return buffer;
     }
 
-    private void WriteLineB(byte[] text)
+    private void WriteLine(byte[] text)
     {
-        byte[] buffer = new byte[text.Length + 1];
-        Array.Copy(text, buffer, text.Length);
-        buffer.SetValue(10, text.Length);
-        Send(buffer);
-    }
-
-    private void Send(byte[] buffer)
-    {
+        string t = Encoding.ASCII.GetString(text);
+        t += "\n";
+        byte[] buffer = Encoding.ASCII.GetBytes(t);
         Sock.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, SendAR, this);
     }
 
