f6a32ad7f0f295e3f8945e95579ce13f280c94bc
emberfarkas <hubing91@163.com>
2016-04-22 11:45:58 +0800

    xx

---
+++ Gate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gate {
+
+    public delegate void ConnectCallback(bool connected);
+    public delegate void RecviveCallback(byte[] data, int start, int length);
+    public delegate void DisconnectCallback(SocketError socketError, PackageSocketError packageSocketError);
+
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Command()
+    {
+
+    }
+}
